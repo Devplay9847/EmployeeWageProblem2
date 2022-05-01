b3993109b65d87e063031e93bbd1e8586822114c
Devplay9847 <devendra.nishad.9847@gmail.com>
2022-05-02 01:15:46 +0530

    Wellcome to Employee wage problem 2

---
+++ EmployeeWage2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO EMPLOYEE WAGE PROBLEM PART 2");
+            Console.ReadLine();
+        }
+    }
+}
